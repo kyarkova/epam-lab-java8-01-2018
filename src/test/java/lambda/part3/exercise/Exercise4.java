04562a952243e970a10cc21012de9c45867b18d8
Kseniia Iarkova <kseniia_iarkova@epam.com>
2018-01-27 23:41:13 +0300

    lambda.part3.exercise4

--- src/test/java/lambda/part3/exercise/Exercise4.java
+++ src/test/java/lambda/part3/exercise/Exercise4.java
@@ -1,10 +1,13 @@
 package lambda.part3.exercise;
 
+import com.google.common.primitives.Chars;
 import lambda.data.Employee;
+import lambda.data.JobHistoryEntry;
 import lambda.part3.example.Example1;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.function.Function;
 
@@ -14,22 +17,39 @@ import static org.junit.Assert.assertEquals;
 public class Exercise4 {
 
     private static class LazyCollectionHelper<T, R> {
+        private final List<T> source;
+        private Function<T, List<R>> function;
+
+        private LazyCollectionHelper(List<T> source, Function<T, List<R>> remapping) {
+            this.source = source;
+            this.function = remapping;
+        }
 
         public static <T> LazyCollectionHelper<T, T> from(List<T> list) {
-            throw new UnsupportedOperationException();
+            return new LazyCollectionHelper<>(list, Collections::singletonList);
         }
 
         public <U> LazyCollectionHelper<T, U> flatMap(Function<R, List<U>> flatMapping) {
-            throw new UnsupportedOperationException();
+            return new LazyCollectionHelper<>(source,
+                    function.andThen(transformUsing(flatMapping)));
         }
 
         public <U> LazyCollectionHelper<T, U> map(Function<R, U> flatMapping) {
-            throw new UnsupportedOperationException();
+            return new LazyCollectionHelper<>(source, function.andThen(transformUsing(
+                    flatMapping.andThen(Collections::singletonList))));
 
         }
 
         public List<R> force() {
-            throw new UnsupportedOperationException();
+            return transformUsing(function).apply(source);
+        }
+
+        private <FROM, TO> Function<List<FROM>, List<TO>> transformUsing(Function<FROM, List<TO>> mapper) {
+            return source -> {
+                List<TO> result = new ArrayList<>();
+                source.forEach(mapper.andThen(result::addAll)::apply);
+                return result;
+            };
         }
     }
 
@@ -37,17 +57,17 @@ public class Exercise4 {
     public void mapEmployeesToCodesOfLetterTheirPositionsUsingLazyFlatMapHelper() {
         List<Employee> employees = Example1.getEmployees();
 
-        List<Integer> codes = null;
-        // TODO              LazyCollectionHelper.from(employees)
-        // TODO                                  .flatMap(Employee -> JobHistoryEntry)
-        // TODO                                  .map(JobHistoryEntry -> String(position))
-        // TODO                                  .flatMap(String -> Character(letter))
-        // TODO                                  .map(Character -> Integer(code letter)
-        // TODO                                  .getMapped();
+        List<Integer> codes =
+                LazyCollectionHelper.from(employees)
+                                    .flatMap(Employee::getJobHistory)
+                                    .map(JobHistoryEntry::getPosition)
+                                    .flatMap(str -> Chars.asList(str.toCharArray()))
+                                    .map(letter -> (int)letter)
+                                    .force();
         assertEquals(calcCodes("dev", "dev", "tester", "dev", "dev", "QA", "QA", "dev", "tester", "tester", "QA", "QA", "QA", "dev"), codes);
     }
 
-    private static List<Integer> calcCodes(String...strings) {
+    private static List<Integer> calcCodes(String... strings) {
         List<Integer> codes = new ArrayList<>();
         for (String string : strings) {
             for (char letter : string.toCharArray()) {
