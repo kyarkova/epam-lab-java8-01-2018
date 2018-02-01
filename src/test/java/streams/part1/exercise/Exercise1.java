6cffac56328abf9baaa6a281aaefd5c87517584a
Kseniia_Iarkova <kseniia_iarkova@epam.com>
2018-02-01 11:05:23 +0300

    streams.part1.exercise1

--- src/test/java/streams/part1/exercise/Exercise1.java
+++ src/test/java/streams/part1/exercise/Exercise1.java
@@ -1,11 +1,13 @@
 package streams.part1.exercise;
 
 import lambda.data.Employee;
+import lambda.data.JobHistoryEntry;
 import lambda.data.Person;
 import lambda.part3.example.Example1;
 import org.junit.Test;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 import static org.junit.Assert.assertEquals;
 
@@ -16,8 +18,12 @@ public class Exercise1 {
     public void findPersonsEverWorkedInEpam() {
         List<Employee> employees = Example1.getEmployees();
 
-        // TODO реализация, использовать Collectors.toList()
-        List<Person> personsEverWorkedInEpam = null;
+        List<Person> personsEverWorkedInEpam = employees.stream()
+                                                        .filter(e -> e.getJobHistory()
+                                                                      .stream()
+                                                                      .anyMatch(jh -> "EPAM".equals(jh.getEmployer())))
+                                                        .map(Employee::getPerson)
+                                                        .collect(Collectors.toList());
 
         List<Person> expected = Arrays.asList(
                 employees.get(0).getPerson(),
@@ -31,8 +37,12 @@ public class Exercise1 {
     public void findPersonsBeganCareerInEpam() {
         List<Employee> employees = Example1.getEmployees();
 
-        // TODO реализация, использовать Collectors.toList()
-        List<Person> startedFromEpam = null;
+        List<Person> startedFromEpam = employees.stream()
+                                                .filter(e -> "EPAM".equals(e.getJobHistory()
+                                                                            .get(0)
+                                                                            .getEmployer()))
+                                                .map(Employee::getPerson)
+                                                .collect(Collectors.toList());
 
         List<Person> expected = Arrays.asList(
                 employees.get(0).getPerson(),
@@ -45,8 +55,10 @@ public class Exercise1 {
     public void findAllCompanies() {
         List<Employee> employees = Example1.getEmployees();
 
-        // TODO реализация, использовать Collectors.toSet()
-        Set<String> companies = null;
+        Set<String> companies = employees.stream()
+                                         .flatMap(e->e.getJobHistory().stream()
+                                                        .map(JobHistoryEntry::getEmployer))
+                                         .collect(Collectors.toSet());
 
         Set<String> expected = new HashSet<>();
         expected.add("EPAM");
@@ -61,8 +73,11 @@ public class Exercise1 {
     public void findMinimalAgeOfEmployees() {
         List<Employee> employees = Example1.getEmployees();
 
-        // TODO реализация
-        Integer minimalAge = null;
+        Integer minimalAge = employees.stream()
+                                      .mapToInt(e->e.getPerson()
+                                                    .getAge())
+                                      .min()
+                                      .orElseThrow(IllegalStateException::new);
 
         assertEquals(21, minimalAge.intValue());
     }
